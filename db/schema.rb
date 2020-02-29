2156c2aee01a5b662de0878e3481db6535ce5d83
Arina Zaitseva <avanero@bk.ru>
2020-02-29 16:22:18 +0700

    model category created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_083646) do
+ActiveRecord::Schema.define(version: 2020_02_29_091951) do
+
+  create_table "categories", force: :cascade do |t|
+    t.integer "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
