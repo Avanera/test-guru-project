c9104f166f7a78ef8077e6da9f0fb835095a30a5
Arina Zaitseva <avanero@bk.ru>
2020-03-01 00:33:50 +0700

    level default - integer 0

--- db/migrate/20200229093902_add_answers_correct_and_tests_level_default.rb
+++ db/migrate/20200229093902_add_answers_correct_and_tests_level_default.rb
@@ -1,6 +1,6 @@
 class AddAnswersCorrectAndTestsLevelDefault < ActiveRecord::Migration[6.0]
   def change
     change_column_default :answers, :correct, from:true, to:false
-    change_column_default :tests, :level, from: nil, to: "0"
+    change_column_default :tests, :level, from: nil, to: 0
   end
 end
