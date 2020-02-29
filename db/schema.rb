b85a6508cdbfbf2a78b8b3578f436d64beac1322
Arina Zaitseva <avanero@bk.ru>
2020-02-29 16:32:10 +0700

    model user created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_092817) do
+ActiveRecord::Schema.define(version: 2020_02_29_093115) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", null: false
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 2020_02_29_092817) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "answers", "questions"
