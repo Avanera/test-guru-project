e53b75fdbd39dfc3d89451ec35c9a5dbcafbac9c
Arina Zaitseva <avanero@bk.ru>
2020-03-06 19:36:19 +0700

    foreign_key corrected

--- db/migrate/20200229082446_create_questions.rb
+++ db/migrate/20200229082446_create_questions.rb
@@ -3,7 +3,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
     create_table :questions do |t|
       t.string :body, null: false
       #t.integer :test_id
-      t.references :test
+      t.references :test, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20200229092343_create_answers.rb
+++ db/migrate/20200229092343_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.boolean :correct, null: false, default: false
-      t.references :question
+      t.references :question, foreign_key: true
       # t.integer :question_id
 
       t.timestamps
