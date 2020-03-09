5bb35f8afb4dfeaf127d26786fc6980422add21f
Arina Zaitseva <avanero@bk.ru>
2020-03-09 19:57:56 +0700

    select -> pluck

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.titles_of_category(category)
     joins("JOIN categories ON tests.category_id = categories.id")
-    .where("categories.title = ?", category).order(title: :desc).select(:title)
+    .where("categories.title = ?", category).order(title: :desc).pluck(:title)
   end
 end
