e7eb0a35e61b66c71f6282ab04139f0f6b7ddbdd
Arina Zaitseva <avanero@bk.ru>
2020-03-13 22:07:42 +0700

    Examples from screencast

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,11 @@
 class TestsController < ApplicationController
 
+  before_action :find_test, only: %i[show]
+  after_action :send_log_message
+  around_action :log_execute_time
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
   def index
     result = ["Class: #{params.class}", "Parameters: #{params.inspect}"]
 
@@ -7,10 +13,55 @@ class TestsController < ApplicationController
   end
 
   def show
-    redirect_to root_path
+    # title = Test.first.title
+    # render inline: '<%= title %>'
+    render inline: '<%= @test.title %>'
   end
 
   def new
 
   end
+
+  def create
+    # result = ["Class: #{params.class}", "Parameters: #{params.inspect}"]
+
+    # render plain: result.join("\n")
+
+    test = Test.create(test_params)
+
+    render plain: test.inspect
+  end
+
+  def search
+    result = ["Class: #{params.class}", "Parameters: #{params.inspect}"]
+
+    render plain: result.join("\n")
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def send_log_message
+    logger.info("Action [#{action_name}] was finished")
+  end
+
+  def log_execute_time
+    start = Time.now
+    yield
+    finish = Time.now - start
+
+    logger.info("Execution time: #{finish * 1000} ms")
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Test was not found'
+  end
 end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  root to: 'tests#index'
+  root 'tests#index'
 
   resources :tests
 
+  get '/tests/:category/:title', to: 'tests#search'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
-
