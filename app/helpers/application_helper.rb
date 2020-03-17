76b511062f80a7e2509032a227ab2637357d0ee5
Arina Zaitseva <avanero@bk.ru>
2020-03-17 23:40:28 +0700

    helpers added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_year
+    Time.zone.now.year
+  end
+
+  def github_link
+    link_to "Test-Guru Project", 'https://github.com/Avanera/test-guru-project/', target: :_blank
+  end
 end
