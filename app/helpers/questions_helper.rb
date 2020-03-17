2ae46ec23a1577e673c67d1a9a83ab2c995b0a32
Arina Zaitseva <avanero@bk.ru>
2020-03-17 23:35:38 +0700

    question_header helper added

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+
+  def question_header(test)
+    if request.url.include? "edit"
+      render plain: "Edit #{@test.title} Question"
+    elsif request.url.include? "new"
+      render plain: "Create New #{@test.title} Question"
+    end
+  end
 end
