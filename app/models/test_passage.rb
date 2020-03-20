37d5b70c66b10ff440fc52e7c394b4e7419ab543
Arina Zaitseva <avanero@bk.ru>
2020-03-20 18:16:51 +0700

    before_validation_set_next_question

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,6 +4,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: "Question", optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
 
   def completed?
     current_question.nil?
@@ -13,12 +14,15 @@ class TestPassage < ApplicationRecord
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
-
-    self.current_question = next_question
+    
     save!
   end
 
   private
+
+  def before_validation_set_next_question
+    self.current_question = next_question
+  end
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
