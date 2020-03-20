de5cf31fd4272f27f9bd187c5b40a5ba603727c8
Arina Zaitseva <avanero@bk.ru>
2020-03-20 19:34:37 +0700

    test_passages.result.html.erb - done

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,21 @@
 module TestPassagesHelper
+  def percent(test_passage)
+    (test_passage.correct_questions.to_f / test_passage.test.questions.count * 100).to_i
+  end
+
+  def show_result
+    if percent(@test_passage) >= 85
+      "Congratulations! You have passed the test."
+    else
+      "You haven't passed the test."
+    end
+  end
+
+  def h2_class
+    if percent(@test_passage) >= 85
+      "green"
+    else
+      "red"
+    end
+  end
 end
