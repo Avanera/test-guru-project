4cad433fd3b189915435bb0b833a15dd5c08bdb5
Arina Zaitseva <avanero@bk.ru>
2020-03-29 22:03:29 +0700

    Devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users, path: 'gurus', path_name: { sign_in: :login, sign_out: :logout}
+
   root 'tests#index'
 
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
   delete :logout, to: 'sessions#destroy'
-
-  resources :users, only: :create
-  resources :sessions, only: :create
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
