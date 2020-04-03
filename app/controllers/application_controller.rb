5b9761771d9601ed6e57336e275545606c1b1a55
Arina Zaitseva <avanero@bk.ru>
2020-04-03 15:20:17 +0700

    localisation in rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_locale
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :deny_access
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   def after_sign_in_path_for(resource)
     if resource.admin?
@@ -21,6 +26,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
 
   def deny_access
     cookies[:original_url] = request.url if cookies[:original_url].blank?
