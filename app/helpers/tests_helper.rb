451c2ba4f1d775b5041ef6d6653f6070b401402b
Arina Zaitseva <avanero@bk.ru>
2020-04-06 19:38:30 +0700

    constant TEST_LEVELS translated

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,21 +1,14 @@
 module TestsHelper
 
-  TEST_LEVELS = { 0 => I18n.t('helpers.label.test.easy'), 1 => I18n.t('helpers.label.test.elementary'), 2 => I18n.t('helpers.label.test.advanced'), 3 => I18n.t('helpers.label.test.hard') }.freeze
+  TEST_LEVELS = {
+    0 => 'easy',
+    1 => 'elementary',
+    2 => 'advanced',
+    3 => 'hard'
+  }.freeze
 
   def test_level(test)
-    TEST_LEVELS[test.level] || I18n.t('helpers.label.test.hero')
-    # case test.level
-    # when 0
-    #   'easy'
-    # when 1
-    #   'elementary'
-    # when 2
-    #   'advanced'
-    # when 3
-    #   'hard'
-    # else
-    #   'hero'
-    # end
+    I18n.t("helpers.label.test.#{TEST_LEVELS[test.level]}") ||
+      I18n.t('helpers.label.test.hero')
   end
-
 end
