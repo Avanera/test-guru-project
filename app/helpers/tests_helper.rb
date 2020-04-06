769673277c7dcfded0c6f4ed064ad86687a11203
Arina Zaitseva <avanero@bk.ru>
2020-04-06 12:17:18 +0700

    def test_level in TestsHelper localization

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,9 +1,9 @@
 module TestsHelper
 
-  TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
+  TEST_LEVELS = { 0 => I18n.t('helpers.label.test.easy'), 1 => I18n.t('helpers.label.test.elementary'), 2 => I18n.t('helpers.label.test.advanced'), 3 => I18n.t('helpers.label.test.hard') }.freeze
 
   def test_level(test)
-    TEST_LEVELS[test.level] || :hero
+    TEST_LEVELS[test.level] || I18n.t('helpers.label.test.hero')
     # case test.level
     # when 0
     #   'easy'
