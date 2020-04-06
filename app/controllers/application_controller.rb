3d82f249d4329ffe604dd52473110fb39a28b8bf
Arina Zaitseva <avanero@bk.ru>
2020-04-06 22:34:08 +0700

    exclude default lang from params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   helper_method :deny_access
 
   def default_url_options
-    { lang: I18n.locale }
+    { lang: I18n.locale == I18n.default_locale ? nil : I18n.locale }
   end
 
   def after_sign_in_path_for(resource)
