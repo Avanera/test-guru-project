5caa6a783a1c6e4161aa4910d56764144a8b2005
Arina Zaitseva <avanero@bk.ru>
2020-04-24 19:26:20 +0700

    add test passage progress bar

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,9 +1,9 @@
 module TestPassagesHelper
   def show_result
     if @test_passage.success?
-      ("<h2 class='green'>#{result_message}<br>Congratulations! You have passed the test.</h2>").html_safe
+      ("<h2 style='color:green'>#{result_message}<br>Congratulations! You have passed the test.</h2>").html_safe
     else
-      ("<h2 class='red'>#{result_message}<br>You haven't passed the test.</h2>").html_safe
+      ("<h2 style='color:red'>#{result_message}<br>You haven't passed the test.</h2>").html_safe
     end
   end
 
