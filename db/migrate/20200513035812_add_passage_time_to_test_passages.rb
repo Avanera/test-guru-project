7b7e5dcd5a8ae75afd333cd5b46845f711de6bab
Arina Zaitseva <avanero@bk.ru>
2020-05-14 18:04:16 +0700

    delete passage_time attribute

--- db/migrate/20200513035812_add_passage_time_to_test_passages.rb
+++
@@ -1,5 +0,0 @@
-class AddPassageTimeToTestPassages < ActiveRecord::Migration[6.0]
-  def change
-    add_column :test_passages, :passage_time, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_035812) do
+ActiveRecord::Schema.define(version: 2020_05_10_095527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,6 @@ ActiveRecord::Schema.define(version: 2020_05_13_035812) do
     t.integer "correct_questions", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "passage_time"
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
